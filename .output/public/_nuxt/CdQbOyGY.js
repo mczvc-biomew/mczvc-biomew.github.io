55d581d5eaffd3f3aca9a574a78ab5f5053ab434
Meldencio Czarlemagne <mczvc@proton.me>
2024-12-09 20:21:19 +0800

    [0.0.12-fix] 12-09-2024 2021H

---
+++ .output/public/_nuxt/CdQbOyGY.js
@@ -0,0 +1,1 @@
+import s from"./eLi1gaVo.js";import{e as o,G as u,n as f,P as m}from"./BfTyt4F8.js";import"./BmlbPuZL.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=m(),{between:n,default:a}=u(),r=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
