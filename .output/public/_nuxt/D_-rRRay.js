9144c5178998a0a5570855be6f7498ca7859097f
Meldencio Czarlemagne <mczvc@proton.me>
2024-12-10 11:54:23 +0800

    regenerate -> 0.0.13

--- .output/public/_nuxt/D_-rRRay.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./oafLT0D_.js";import"./zjB1x_6Z.js";export{m as default};
--- .output/public/_nuxt/DybVo5nx.js
+++
@@ -1,1 +0,0 @@
-import s from"./C8XJ9q6h.js";import{e as o,G as u,n as f,P as m}from"./zjB1x_6Z.js";import"./BmlbPuZL.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=m(),{between:n,default:a}=u(),r=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
