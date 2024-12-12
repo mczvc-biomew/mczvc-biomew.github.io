f65a15c1a60d71a2f978572b84b5a45ec28ee213
Meldencio Czarlemagne <mczvc@proton.me>
2024-12-12 21:05:08 +0800

    regenerate -> 0.0.15

--- .output/public/_nuxt/BWninhlZ.js
+++
@@ -1,1 +0,0 @@
-import{e,o,c as a}from"./CnVImnGZ.js";const t="/music/KMKZ.mp3",p=e({__name:"Music",setup(c){return(n,r)=>(o(),a("embed",{id:"audio",src:t,type:"audio/mp3",autoplay:"true"}))}});export{p as _};
--- .output/public/_nuxt/CqCdC4Li.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./F7uDCH29.js";import"./CnVImnGZ.js";import"./C-v3KzvZ.js";import"./BmlbPuZL.js";import"./BNaPUVCS.js";export{o as default};
