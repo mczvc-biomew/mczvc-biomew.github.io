445f5706cfcc6fccd1718293024eb7172045bd30
Meldencio Czarlemagne <mczvc@proton.me>
2024-12-16 07:07:45 +0800

    regenerate -> 0.0.15

--- .output/public/_nuxt/JGkIj-nR.js
+++
@@ -1,1 +0,0 @@
-import{d as a,o,c as n,q as r,t as l,C as s,D as c}from"./BIpVtYVe.js";const p=a({__name:"DocumentContainer",props:{text:{type:String},classNames:{type:String}},setup(e){return(t,i)=>(o(),n("div",{class:c([e.classNames])},[r(l(e.text)+" ",1),s(t.$slots,"default"),s(t.$slots,"footer")],2))}});export{p as default};
