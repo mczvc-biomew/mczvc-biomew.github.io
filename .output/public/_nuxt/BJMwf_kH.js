d5a4a2111fcdfb050b8ec2d40e27aacd1830fc4b
Meldencio Czarlemagne <mczvc@proton.me>
2025-04-21 22:11:12 +0800

    [0.0.20] 2025-04-21 (2210H)

---
+++ .output/public/_nuxt/BJMwf_kH.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./Bg1J-4VW.js";import{_ as t,o as c,b as s}from"./-wh18xAP.js";const n={};function r(a,_){const e=o;return c(),s(e,{src:"https://utteranc.es/client.js",repo:"mczvc-biomew/mczvc-biomew.github.io","issue-term":"pathname",label:"good first issue",theme:"github-dark",crossorigin:"anonymous",async:"true"})}const p=t(n,[["render",r]]);export{p as default};
